bfecd1b261d3d86a157c702649af58900cc672b7
delasimensah <mensadelasi@gmail.com>
2022-06-16 15:38:59 +0000

    completed navbar and footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Logo from "../assets/images/Logo-1.png";
+
+// mui
+import { Box, Stack, Typography } from "@mui/material";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack
+        gap="40px"
+        sx={{ alignItems: "center" }}
+        flexWrap="wrap"
+        px="40px"
+        pt="24px"
+      >
+        <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
+      </Stack>
+
+      <Typography
+        variant="h5"
+        sx={{ fontSize: { lg: "24px", xs: "20px" } }}
+        mt="41px"
+        textAlign="center"
+        pb="40px"
+        fontFamily="Nunito"
+      >
+        Developed by Delasi Mensah
+      </Typography>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/Logo.png";
+
+// mui
+import { Stack } from "@mui/material";
+
+const color = "#3a1212";
+
+const linkStyle = {
+  textDecoration: "none",
+  color,
+};
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: {
+          sm: "123px",
+          xs: "40px",
+        },
+        mt: {
+          sm: "32px",
+          xs: "20px",
+        },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
+      <Link to="/">
+        <img
+          src={logo}
+          alt=""
+          style={{ width: "45px", height: "45px", margin: "0px 18px" }}
+        />
+      </Link>
+
+      <Stack
+        direction="row"
+        gap="40px"
+        fontSize="20px"
+        fontFamily="Nunito"
+        alignItems="flex-end"
+      >
+        <Link
+          to="/"
+          style={{ ...linkStyle, borderBottom: "3px solid #ff2625" }}
+        >
+          Home
+        </Link>
+        <a href="#exercises" style={linkStyle}>
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
