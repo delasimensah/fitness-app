ad63ced4e45d69fbfaa27afd67a3f6f00f265ce5
delasimensah <mensadelasi@gmail.com>
2022-06-16 15:17:13 +0000

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+// mui
+import { Box } from "@mui/material";
+
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// pages
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+
 import "./App.css";
 
 const App = () => {
-  return <div>Fitness App</div>;
+  return (
+    <Box width="400px" m="auto" sx={{ width: { xl: "1488px" } }}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
